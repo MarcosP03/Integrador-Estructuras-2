333fc610a4f9528138096d3eff369ca3a19c621b
Marcos Portela <marcossportelaa@proton.me>
2025-05-20 12:48:13 -0300

    Cambios en Main y Estructuras
    Defini las estructuras que utilizaremos y el main hice un menu atado con alambres, esta sujeto a muchos cambios

--- Estructuras.h
+++ Estructuras.h
@@ -1,16 +1,61 @@
 #ifndef ESTRUCTURAS_H
 #define ESTRUCTURAS_H
 
-typedef struct ProcesadasLES {
+typedef struct PilaEncuestas {
+	int idEncuesta;
+	char denominacion[50];
+	int encuestaMes;
+	int anio;
+	int procesada;
+	int cantidadPreguntas;
+	struct LESPreguntas* topePreguntas;
+	struct LESProcesadas* topeProcesadas;
+	struct PilaEncuestas* siguiente;
+} PilaEncuestas;
+
+typedef struct LESPreguntas {
+	int idEncuesta;
+	int idPregunta;
+	char pregunta[100];
+	float ponderacion;
+	struct LECRespuesta* topeRespuestas;
+	struct LESPreguntas* siguiente;
+} LESPreguntas;
+
+typedef struct LECRespuesta {
+	int idRespuesta;
+	int idPregunta;
+	int NumRespuesta;
+	char respuesta[50];
+	float ponderacion;
+	int elegida;
+	struct LECRespuesta* siguiente;
+} LECRespuesta;
+
+typedef struct LESProcesadas {
 	int idEncuesta;
 	long fecha;
 	int idEncuestador;
 	long idEncuestaRespondida;
 	float ponderacion;
-	struct ProcesadasLES* siguiente;
-};
+	struct LESProcesadas* siguiente;
+} LESProcesadas;
 
+typedef struct LESEncuestadores {
+	int idEncuestador;
+	char NombreEncuestador[50];
+	struct LESEncuestadores* siguiente;
+} LESEncuestadores;
 
+typedef struct LESGestorIDs {
+	int idGestor;
+	int idEncuesta;
+	int idPregunta;
+	int idRespuesta;
+	int idEncuestador;
+	int idEncuestaProcesada;
+	struct LESGestorIDs* siguiente;
+} LESGestorIDs;
 
 
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
+#include <windows.h>
 
 #include "Estructuras.h"
 #include "Funciones.h"
 
 int main() {
+	int opc;
+	long clave;
 	
+	system("cls");
+	printf("\t-- Bienvenido --\t\n \nIntroduza su clave de ingreso: ");
+	scanf("%ld", &clave);
+
+	do {
+		system("cls");
+		printf("Seleccione la seccion en que desea trabajar: \n");
+		printf("1. Encuestadores\n");
+		printf("2. Encuestas\n");
+		printf("3. Procesar Encuestas\n");
+		printf("4. Salir\n");
+		printf("Opcion: ");
+		scanf("%d", &opc);
+		switch(opc) {
+			case 1:
+				opc = 4;
+				break;
+			case 2:
+				opc = 4;
+				break;
+			case 3:
+				opc = 4;
+				break;
+			case 4:
+				printf("\nSaliendo....");
+				Sleep(1500);
+				break;
+			default: 
+				printf("\nOpcion Invalida\n");
+				break;
+		}
+	} while(opc != 4);
+	
+	return 0;
 }
